a93b3d33731b6ae6af4e5aabaef566d5cad9e9df
Holger Rosencrantz <holgros@gmail.com>
2025-01-16 10:43:14 +0100

    Mer om JavScript och DOM

--- v03/script.js
+++ v03/script.js
@@ -6,4 +6,32 @@ btn.onclick = function (event) {
 let btn2 = document.getElementById("myButton2");
 btn2.addEventListener("click", function (event) {
   console.log("Klickade på knappen igen.");
+  console.log(event); // event-objektet innehåller en massa infomation
+});
+
+// lyssna till tangentbordstryckningar
+window.addEventListener("keypress", (event) => {
+  console.log("Tangent nedtryckt", event.key);
+});
+
+// Hämta referens till alla element med en klass.
+// getElementsByClassName() returnerar en collection att loopa över.
+let buttons = document.getElementsByClassName("buy");
+
+// Loopa igenom alla knappar.
+for (let i = 0; i < buttons.length; i++) {
+  const button = buttons[i];
+  // Lägg till eventlyssnare på knappen.
+  button.addEventListener("click", function (event) {
+    console.log("En köpknapp klickades.");
+  });
+}
+
+// identifiera en div i DOM:en och skriv ut till denna
+// lyssna till mushändelsen mousemove
+window.addEventListener("mousemove", (event) => {
+  //console.log(`Musen är nu på koordinaterna (${event.clientX},${event.clientY})`);
+  let position = document.getElementById("position");
+  // Skriv ut.
+  position.innerHTML = `Musen är nu på koordinaterna (${event.clientX},${event.clientY})`;
 });
